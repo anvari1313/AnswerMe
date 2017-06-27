70caae47a2f9d3cf725552b7352d666d049fbbb1
Ahmad Anvari <ahmad.anv@gmail.com>
2017-06-27 20:12:37 +0430

    Adding some fake actions to test

---
+++ api/controllers/AnswerController.js
@@ -0,0 +1,19 @@
+/**
+ * AnswerController
+ *
+ * @description :: Server-side logic for managing answers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  'new': function (req, res) {
+
+  },
+
+  addComment:function (req, res) {
+
+  }
+
+};
+
