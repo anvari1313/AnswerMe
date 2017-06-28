d3af45e47bbbbbfe309bf98de252ba037daa2bb8
Ahmad Anvari <ahmad.anv@gmail.com>
2017-06-29 00:03:38 +0430

    Listing all answers with responser

--- api/controllers/QuestionController.js
+++ api/controllers/QuestionController.js
@@ -7,20 +7,25 @@
 
 module.exports = {
 
-	'new' : function (req, res) {
-	  console.log(req.user.fname);
-	  var tags = [];
-	  if (req.query.tag)
-	    tags = req.query.tag;
-	  return res.view('question/new', {tags: tags});
+  'new': function (req, res) {
+    console.log(req.user.fname);
+    var tags = [];
+    if (req.query.tag)
+      tags = req.query.tag;
+    return res.view('question/new', {tags: tags});
   },
 
   'create': function (req, res) {
 
-	  var question = req.params.all();
+    var question = req.params.all();
 
-	  console.log();
-    Question.create({title:question.title, tags:question.tags.split(','), text:question.text, asker:req.user}, function (err, result) {
+    console.log();
+    Question.create({
+      title: question.title,
+      tags: question.tags.split(','),
+      text: question.text,
+      asker: req.user
+    }, function (err, result) {
       if (err)
         return res.json(err);
       else
@@ -30,32 +35,67 @@ module.exports = {
     // return res.json({que:question, user: req.user});
   },
 
-  index:function (req, res) {
-	  console.log('this');
-	  Question.find().exec(function (err, questions) {
+  index: function (req, res) {
+    console.log('this');
+    Question.find().exec(function (err, questions) {
       if (err)
-        return res.view('500', {data:err});
-      else{
-        return res.view('question/index',{questions:questions});
+        return res.view('500', {data: err});
+      else {
+        return res.view('question/index', {questions: questions});
         // return res.json({questions:questions});
       }
     });
   },
 
-  'question':function (req, res, next) {
+  'question': function (req, res, next) {
     var question_id = req.param('question_id');
-	  Question.findOne({id: question_id}).exec(function (err1, question) {
-	    Answer.find({question: question.id}).exec(function (err2, answers) {
-        if (err1 || err2)
-          return res.view('500', {data:err});
-        if (! question)
-          next();
-        else{
-          question.answers = answers;
-          return res.view('question/singleQuestion', {question: question});
-        }
+    Question.findOne({id: question_id}).exec(function (err1, question) {
+      if (!question)
+        return next();
+      Answer.find({question: question.id}).exec(function (err2, answers) {
+          if (err1 || err2)
+            return res.view('500', {data: err});
+          else {
+            var counter = 0;
+            answers.forEach(function (answer, index, answers) {
 
-      });
+              User.findOne({id: answer.responser}, function (err, user) {
+                answer._responser = {fname: 'hell', lname: 'go'};
+                if (err)
+                  return res.view('500', {data: err});
+                else {
+                  counter ++;
+                  answer.responser = user;
+
+                  if (counter === answers.length){
+                    question.answers = answers;
+                    return res.view('question/singleQuestion', {question: question});
+                  }
+                }
+              });
+            });
+
+            // var questionRequested;
+            // Question.findOne({id: question_id})
+            //   .then(function (question) {
+            //     if (! question)
+            //       return next();
+            //     questionRequested = question;
+            //     return Answer.find({question: question_id});
+            //   })
+            //   .then(function (answers) {
+            //     answers.forEach(function (answer, index) {
+            //
+            //     })
+            //   })
+            //   .catch(function (err) {
+            //
+            //   })
+          }
+
+        }
+      )
+      ;
 
     });
   }
