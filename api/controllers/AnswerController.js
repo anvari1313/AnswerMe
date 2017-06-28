05ab6f4b99e0846abc845807c1555cf464a2fc57
Ahmad Anvari <ahmad.anv@gmail.com>
2017-06-28 19:40:02 +0430

    Add create action

--- api/controllers/AnswerController.js
+++ api/controllers/AnswerController.js
@@ -7,7 +7,25 @@
 
 module.exports = {
 
-  'new': function (req, res) {
+  'create': function (req, res) {
+    var question_id = req.param('question_id');
+    var user = req.user;
+    var text = req.params.all().text;
+    Question.findOne({id:question_id}, function (err, question) {
+      Answer.create({text:text,question: question,responser: user}, function (err, answer) {
+        if (err)
+          return res.json(err);
+        else
+        {
+          question.answers.add(answer);
+          question.save(function (err) {
+            if (err)
+              return res.json(err);
+          });
+          return res.json(answer);
+        }
+      })
+    });
 
   },
 
